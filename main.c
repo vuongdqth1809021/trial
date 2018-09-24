00633037c00ec0cfcbe2d3640d40f1a919963749
vuongdqth1809021 <43413157+vuongdqth1809021@users.noreply.github.com>
2018-09-24 20:16:50 +0700

    first commit

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-
+int hello() {
+    printf("hello!\n");
+}
 int main() {
     printf("Hello, World!\n");
     printf("Hello C!\n");
+    hello();
     return 0;
+}
+int a = 1;
+int b = 3;
+int tongHaiSo(int a ,int b){
+ return a + b;
 
 }
+
+
+
+
+
+
