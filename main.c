c339d47d1a09d840bb9264596ccd16b81ca206f6
vuongdqth1809021 <43413157+vuongdqth1809021@users.noreply.github.com>
2018-09-24 22:51:07 +0700

    hello

--- main.c
+++ main.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-int hello() {
-    printf("hello!\n");
-}
+
+
+
+
 int main() {
-    printf("Hello, World!\n");
-    printf("Hello C!\n");
-    hello();
+    int num;
+    printf("vui long nhap mot so:");
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+        printf("number is even");
+    }
     return 0;
 }
-int a = 1;
-int b = 3;
-int tongHaiSo(int a ,int b){
- return a + b;
-
-}
-
-
-
-
-
-
