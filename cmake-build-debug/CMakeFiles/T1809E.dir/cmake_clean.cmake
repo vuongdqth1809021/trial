38b80ed9bc40e0c71302efd03dc870baea58f153
vuongdqth1809021 <43413157+vuongdqth1809021@users.noreply.github.com>
2018-09-24 18:20:35 +0700

    hello C

---
+++ cmake-build-debug/CMakeFiles/T1809E.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/T1809E.dir/main.c.obj"
+  "T1809E.pdb"
+  "T1809E.exe"
+  "T1809E.exe.manifest"
+  "libT1809E.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/T1809E.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
