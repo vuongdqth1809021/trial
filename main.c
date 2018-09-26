75ec80612f940ac76c168a0fcc656cf294ffd165
- <->
2018-09-26 21:21:53 +0700

    demo ngày 26/9/2018

--- main.c
+++ main.c
@@ -3,21 +3,32 @@
 
 //khai bao ham cong
 
-int tong(int a, int b) {
-    int c = a + b;
-    return c;
+int tong(int a, int b){
+    return a + b;
 
+}
+int hieu(int a ,int b) {
+return a - b;
 }
 int main() {
-    int a, b, tong1, luaChon;
-    printf("so thu nhat la:");
+    int a, b, tong1,hieu1, luaChon;
+   tong1 = tong( a,  b);
+   hieu1 = hieu( a,  b);
+    printf("So thu nhat la : ");
     scanf("%d", &a);
-    printf("so thu hai la");
+    printf("So thu hai la : ");
     scanf("%d", &b);
+    printf("Lua chon cua ban la(1|2|3|4) \n");
+    scanf("%d", &luaChon);
+    if (luaChon == 1) {
+        printf("Chon phep cong .");
+        printf("Tong cua hai so la : ");printf("%d",tong1);
+    }  else if (luaChon == 2) {
+        printf("Chon phep tru .");
+        printf("Hieu cua hai so la : ");printf("%d",hieu1);
+    }   else {
+        printf("Lua chon sai .");
+    }
 
-    tong1 = tong(a, b);
-    printf("Tong cua hai so %d va %d la %d", a, b, tong1);
     return 0;
 }
-
-
