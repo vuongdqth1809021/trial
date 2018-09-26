ba5a17306150c0c75337138b256ab371e4b9fe1e
- <->
2018-09-26 19:47:18 +0700

    first commit

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 
+//khai bao ham cong
 
+int tong(int a, int b) {
+    int c = a + b;
+    return c;
 
+}
 int main() {
-    int num;
-    printf("vui long nhap mot so:");
-    scanf("%d", &num);
-    if (num % 2 == 0) {
-        printf("number is even");
-    }
+    int a, b, tong1, luaChon;
+    printf("so thu nhat la:");
+    scanf("%d", &a);
+    printf("so thu hai la");
+    scanf("%d", &b);
+
+    tong1 = tong(a, b);
+    printf("Tong cua hai so %d va %d la %d", a, b, tong1);
     return 0;
 }
+
+
