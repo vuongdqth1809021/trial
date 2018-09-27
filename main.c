f7ea0ddb7e24a44b4cc44dbb0bf43ef0cddae335
- <->
2018-09-27 18:22:15 +0700

    demo ngay 27/9/2018

--- main.c
+++ main.c
@@ -8,27 +8,45 @@ int tong(int a, int b){
 
 }
 int hieu(int a ,int b) {
-return a - b;
+    return a - b;
+}
+int thuong(int a ,int b) {
+    return a / b;
+}
+int tich(int a ,int b) {
+    return a * b;
 }
 int main() {
-    int a, b, tong1,hieu1, luaChon;
-   tong1 = tong( a,  b);
-   hieu1 = hieu( a,  b);
+    int a, b, luaChon;
     printf("So thu nhat la : ");
     scanf("%d", &a);
     printf("So thu hai la : ");
     scanf("%d", &b);
-    printf("Lua chon cua ban la(1|2|3|4) \n");
+    printf("Lua chon cua ban la 1 neu la cong ,2 neu la tru ,3 neu la chia ,4 neu la nhan(1|2|3|4) \n");
     scanf("%d", &luaChon);
-    if (luaChon == 1) {
+     if (luaChon == 1) {
         printf("Chon phep cong .");
-        printf("Tong cua hai so la : ");printf("%d",tong1);
+        printf("Tong cua hai so %d va %d la %d", a, b, tong(a ,b));
+
     }  else if (luaChon == 2) {
         printf("Chon phep tru .");
-        printf("Hieu cua hai so la : ");printf("%d",hieu1);
-    }   else {
-        printf("Lua chon sai .");
-    }
+        printf("Hieu cua hai so %d va %d la %d", a, b, hieu(a ,b));
+    }  else if (luaChon == 3) {
+        printf("Chon phep chia .");
+        printf("Thuong cua hai so %d va %d la %d", a, b, thuong(a ,b));
 
+         if (b == 0) {
+             printf("Phep tinh khong dung");
+         }
+         else if(luaChon == 4) {
+             printf("Chon phep nhan .");
+             printf("Tich cua hai so %d va %d la %d", a, b, tich(a ,b));
+         }
+         if (luaChon > 4) {
+             printf()
+         }
+
+
+     }
     return 0;
 }
